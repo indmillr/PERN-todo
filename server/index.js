65461466d3bc30e38ab7602f954ca100b9881ac2
Ian Miller <theiandmiller@gmail.com>
2022-04-14 06:33:22 -0500

    init db.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// middleware
+// ----- MIDDLEWARE ----- //
 app.use(cors());
 app.use(express.json());
 
-app.listen(5432, () => {
+app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
