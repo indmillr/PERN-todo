f6b97bfb1c1ad540d148472b936f99a6157ed7ac
Ian Miller <theiandmiller@gmail.com>
2022-05-09 05:44:33 -0500

    begin creating routes

--- server/db.js
+++ server/db.js
@@ -1,8 +1,7 @@
 const Pool = require("pg").Pool;
-
 const pool = new Pool({
   user: "postgres",
-  password: "pickleweasel",
+  password: "postgres",
   host: "localhost",
   port: 5432,
   database: "perntodo",
--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// ROUTES
+
+// create a todo
+
+// get all todos
+
+// get a todo
+
+// updates a todo
+
+// delete a todo
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
