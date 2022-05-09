e6a506c75fe74a2a35729fd2f9846a02627b871d
Ian Miller <theiandmiller@gmail.com>
2022-05-09 05:20:41 -0500

    update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// ----- MIDDLEWARE ----- //
+// Middleware
 app.use(cors());
 app.use(express.json());
 
